0b07242a2b8400d011f35e30e57270e5fe2ad39e
Adam Fahie <adam.fahie@gmail.com>
2017-05-14 16:30:11 +0100

    Adam - Basic unit test around MaxPairwiseProduct algorithm

--- test/algorithmic/toolbox/week/one/MaxPairwiseProductTest.java
+++ test/algorithmic/toolbox/week/one/MaxPairwiseProductTest.java
@@ -4,14 +4,25 @@ package algorithmic.toolbox.week.one;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
 
 public class MaxPairwiseProductTest {
 
     @Rule
     public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
+    @Rule
+    public final TextFromStandardInputStream systemInMock = emptyStandardInputStream();
+
+    private final String[] emptyArray = {};
+
     @Test
     public void shouldReturnCorrectResultOnBaseCase() {
-        MaxPairwiseProduct.main();
+        systemInMock.provideLines("3", "1 2 3");
+        MaxPairwiseProduct.main(emptyArray);
+        assertEquals("6", systemOutRule.getLog().trim());
     }
 }
