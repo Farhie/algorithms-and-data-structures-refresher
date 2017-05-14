5bb1215ed9cbed308248ab1cd573dd66bf92053d
Adam Fahie <adam.fahie@gmail.com>
2017-05-14 15:30:02 +0100

    Adam - Add failing test for PairWiseProduct

---
+++ test/algorithmic/toolbox/week/one/MaxPairwiseProductTest.java
@@ -0,0 +1,14 @@
+package algorithmic.toolbox.week.one;
+
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
+
+public class MaxPairwiseProductTest {
+
+    @Test
+    public void test() {
+        fail();
+    }
+}
