68e4bae58c3c8b5d42e5e4ceb733c20d002827d2
Adam Fahie <adam.fahie@gmail.com>
2017-05-19 17:18:15 +0100

    Adam - GCD calculated using euclidean algorithm instead of naive solution

--- src/algorithmic/toolbox/week/two/gcd/GCD.java
+++ src/algorithmic/toolbox/week/two/gcd/GCD.java
@@ -3,6 +3,8 @@ package algorithmic.toolbox.week.two.gcd;
 import java.util.Scanner;
 
 public class GCD {
+
+    @Deprecated
     private static int gcd_naive(int a, int b) {
         int current_gcd = 1;
         for (int d = 2; d <= a && d <= b; ++d) {
@@ -16,11 +18,18 @@ public class GCD {
         return current_gcd;
     }
 
+    private static int gcd_euclidean(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd_euclidean(b, a % b);
+    }
+
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-        System.out.println(gcd_naive(a, b));
+        System.out.println(gcd_euclidean(a, b));
     }
 }
