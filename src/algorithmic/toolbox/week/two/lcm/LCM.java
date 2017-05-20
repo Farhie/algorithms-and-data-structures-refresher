df90ec984d35251dff54e104395688ef3bed0394
Adam Fahie <adam.fahie@gmail.com>
2017-05-20 13:07:53 +0100

    Adam - more eficient implementation of Lowest Common Multiple

--- src/algorithmic/toolbox/week/two/lcm/LCM.java
+++ src/algorithmic/toolbox/week/two/lcm/LCM.java
@@ -3,6 +3,8 @@ package algorithmic.toolbox.week.two.lcm;
 import java.util.Scanner;
 
 public class LCM {
+
+    @Deprecated
     private static long lcm_naive(int a, int b) {
         for (long l = 1; l <= (long) a * b; ++l)
             if (l % a == 0 && l % b == 0)
@@ -11,11 +13,40 @@ public class LCM {
         return (long) a * b;
     }
 
+    private static long lowestCommonMultiple(long valueA, long valueB) {
+        long largestNumber = valueA < valueB ? valueB : valueA;
+
+        long[] multiplesOfValueA = new long[(int) largestNumber];
+
+        for (int i = 1; i * valueA <= valueA * valueB; i++) {
+            multiplesOfValueA[i] = i * valueA;
+        }
+
+        for (int i = 1; i * valueB < valueA * valueB; i++) {
+            if(valueHasBeenSeenAsMultipleOfA((i * valueB), multiplesOfValueA)){
+                return i * valueB;
+            }
+        }
+
+        return valueA * valueB;
+    }
+
+    private static boolean valueHasBeenSeenAsMultipleOfA(long value, long[] multiplesOfValueA) {
+        for(long multipleOfA : multiplesOfValueA) {
+            if (multipleOfA == value) {
+                return true;
+            } else if(multipleOfA > value) {
+                return false;
+            }
+        }
+        return false;
+    }
+
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
 
-        System.out.println(lcm_naive(a, b));
+        System.out.println(lowestCommonMultiple(a, b));
     }
 }
